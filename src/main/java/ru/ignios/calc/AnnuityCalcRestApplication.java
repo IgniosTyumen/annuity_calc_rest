c15d43883ae0ca82af9261b7c7648ec5d5afa8c3
IgniosTyumen <rfwbr2@mail.ru>
2019-07-11 20:24:47 +0500

    initial commit

---
+++ src/main/java/ru/ignios/calc/AnnuityCalcRestApplication.java
@@ -0,0 +1,13 @@
+package ru.ignios.calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnuityCalcRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnuityCalcRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ignios/calc/AnnuityCalcRestApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.ignios.calc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnnuityCalcRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
