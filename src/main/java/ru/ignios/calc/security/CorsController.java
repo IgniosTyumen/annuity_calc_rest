f48d2ac5edbcdc78b0d64f4e24f611525d76076d
IgniosTyumen <rfwbr2@mail.ru>
2019-07-13 20:13:45 +0500

    EDITED switched cors policy to https://annuity-calculator.herokuapp.com/

--- src/main/java/ru/ignios/calc/security/CorsController.java
+++ src/main/java/ru/ignios/calc/security/CorsController.java
@@ -15,7 +15,7 @@ public class CorsController {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://annuity-calculator.herokuapp.com")
                         .allowCredentials(true);
             }
         };
